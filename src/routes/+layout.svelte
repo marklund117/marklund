b89943a06f5887eaa402f805a9680a36df5fc330
Mark Lund <marklund117@gmail.com>
2024-12-31 10:56:46 -0700

    favicon and some mobile styling for the menu bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,27 +7,28 @@
 <AppShell class="bg-surface-100">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar background="bg-surface-200" shadow="shadow-lg">
+		<AppBar background="bg-white" shadow="shadow-lg">
 			<svelte:fragment slot="lead">
 				<a href="/">
-				<h1 class="h1"><span class="gradient-heading font-bold font-mono">marklund.tech</span></h1>
+				<h1 class="h1 text-[32px] hidden md:block"><span class="gradient-heading font-bold font-mono">marklund.tech</span></h1>
+				<img class="max-h-[48px] md:hidden" src="favicon.png" alt="Mark Lund" />
 				</a>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
-					class="btn btn-sm bg-gradient-to-br variant-gradient-primary-secondary"
+					class="btn btn-sm bg-gradient-to-br variant-gradient-primary-secondary w-[80px] h-[48px] md:h-[32px]"
 					href="/projects"
 				>
 					Projects
 				</a>
 				<a
-					class="btn btn-sm bg-gradient-to-br variant-gradient-primary-secondary"
+					class="btn btn-sm bg-gradient-to-br variant-gradient-primary-secondary w-[80px] h-[48px] md:h-[32px]"
 					href="/resume"
 				>
 					Resume
 				</a>
 				<a
-					class="btn btn-sm bg-gradient-to-br variant-gradient-primary-secondary"
+					class="btn btn-sm bg-gradient-to-br variant-gradient-primary-secondary w-[80px] h-[48px] md:h-[32px]"
 					href="/blog"
 				>
 					Blog
