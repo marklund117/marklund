d2ed7350e04bc6a3fc58287fb38e2d7b83b56152
Mark Lund <marklund117@gmail.com>
2024-12-31 15:34:14 -0700

    load animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,34 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { fly, fade, crossfade } from 'svelte/transition';
+	import { cubicOut } from 'svelte/easing';
+
+	let showName = false
+	let showText = false
+	let showButtons = false
+
+	onMount(() => {
+		// onmount logic
+		setTimeout(() => {
+			showButtons = true
+		}, 100)
+	});
+</script>
+
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<div class="text-[50px] flex flex-row">
-		<p class="text-secondary-500 mr-4">Mark</p><p class="text-primary-500">Lund</p>
+		{#if showButtons}
+		<div class="text-[50px] flex flex-row" in:fade={{duration: 500, delay: 0}}>
+			<p class="text-secondary-500 mr-4">Mark</p>
+			<p class="text-primary-500">Lund</p>
 		</div>
-		<p class="text-black">Web Designer // Web Developer // User Experience</p>
+		<p class="text-black" in:fade={{duration: 500, delay: 100}}>Web Designer // Web Developer // User Experience</p>
 		<div class="flex justify-center space-x-2">
 			<a
 				class="btn bg-gradient-to-br variant-gradient-primary-secondary w-[120px] h-[48px] md:h-[32px]"
 				href="/projects"
 				rel="noreferrer"
+				in:fly={{ y: 20, duration: 500, delay: 200 }}
 			>
 				Projects
 			</a>
@@ -16,9 +36,11 @@
 				class="btn bg-gradient-to-br variant-gradient-primary-secondary w-[120px] h-[48px] md:h-[32px]"
 				href="/blog"
 				rel="noreferrer"
+				in:fly={{ y: 20, duration: 500, delay: 300 }}
 			>
 				Blog
 			</a>
 		</div>
+		{/if}
 	</div>
 </div>
