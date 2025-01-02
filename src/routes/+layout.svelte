6a94fe55b0f199afe724d509ea2d1c5ee5cf8a4a
Mark Lund <marklund117@gmail.com>
2025-01-01 17:33:54 -0700

    significant visual changes, modified header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,39 +7,43 @@
 <AppShell class="bg-surface-100">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar background="bg-white" shadow="shadow-lg">
-			<svelte:fragment slot="lead">
-				<a href="/">
-				<h1 class="h1 text-[32px] hidden md:block"><span class="gradient-heading font-bold font-mono">marklund.tech</span></h1>
-				<img class="max-h-[48px] md:hidden" src="favicon.png" alt="Mark Lund" />
-				</a>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm bg-gradient-to-br variant-gradient-primary-secondary w-[80px] h-[48px] md:h-[32px]"
-					href="/projects"
-				>
-					Projects
-				</a>
-				<a
-					class="btn btn-sm bg-gradient-to-br variant-gradient-primary-secondary w-[80px] h-[48px] md:h-[32px]"
-					href="/resume"
-				>
-					Resume
-				</a>
-				<a
-					class="btn btn-sm bg-gradient-to-br variant-gradient-primary-secondary w-[80px] h-[48px] md:h-[32px]"
-					href="/blog"
-				>
-					Blog
-				</a>
-			</svelte:fragment>
-		</AppBar>
+		<header class="bg-white shadow-lg items-center h-[64px] flex flex-row justify-center pr-4 pl-4">
+			<div class="w-full lg:w-[70rem] flex flex-row justify-between items-center">
+				<div>
+					<a href="/">
+						<h1 class="h1 text-[32px] hidden md:block">
+							<span class="gradient-heading font-bold font-mono">marklund.tech</span>
+						</h1>
+						<img class="max-h-[48px] md:hidden" src="favicon.png" alt="Mark Lund" />
+					</a>
+				</div>
+				<div>
+					<a
+						class="btn btn-sm bg-gradient-to-br variant-gradient-primary-secondary w-[80px] h-[48px] md:h-[32px]"
+						href="/projects"
+					>
+						Projects
+					</a>
+					<a
+						class="btn btn-sm bg-gradient-to-br variant-gradient-primary-secondary w-[80px] h-[48px] md:h-[32px]"
+						href="/resume"
+					>
+						Resume
+					</a>
+					<a
+						class="btn btn-sm bg-gradient-to-br variant-gradient-primary-secondary w-[80px] h-[48px] md:h-[32px]"
+						href="/blog"
+					>
+						Blog
+					</a>
+				</div>
+			</div>
+		</header>
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<slot />
-		<footer class="bg-neutral-900 text-neutral-100 text-center p-4">
-			<p>Footer test content</p>
-			<p>&copy; 2025 Mark Lund</p>
-		</footer>
+	<footer class="bg-neutral-900 text-neutral-100 text-center p-4">
+		<p>Footer test content</p>
+		<p>&copy; 2025 Mark Lund</p>
+	</footer>
 </AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,14 +18,14 @@
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
 		{#if showButtons}
-		<div class="text-[50px] flex flex-row" in:fade={{duration: 500, delay: 0}}>
+		<div class="text-[72px] flex flex-row mb-4" in:fade={{duration: 500, delay: 0}}>
 			<p class="text-secondary-500 mr-4">Mark</p>
 			<p class="text-primary-500">Lund</p>
 		</div>
-		<p class="text-black" in:fade={{duration: 500, delay: 100}}>Web Designer // Web Developer // User Experience</p>
+		<p class="text-black" in:fade={{duration: 500, delay: 100}}>Web Design // Web Development // User Experience</p>
 		<div class="flex justify-center space-x-2">
 			<a
-				class="btn bg-gradient-to-br variant-gradient-primary-secondary w-[120px] h-[48px] md:h-[32px]"
+				class="btn bg-gradient-to-br variant-gradient-primary-secondary w-[150px] h-[48px] md:h-[32px]"
 				href="/projects"
 				rel="noreferrer"
 				in:fly={{ y: 20, duration: 500, delay: 200 }}
@@ -33,7 +33,7 @@
 				Projects
 			</a>
 			<a
-				class="btn bg-gradient-to-br variant-gradient-primary-secondary w-[120px] h-[48px] md:h-[32px]"
+				class="btn bg-gradient-to-br variant-gradient-primary-secondary w-[150px] h-[48px] md:h-[32px]"
 				href="/blog"
 				rel="noreferrer"
 				in:fly={{ y: 20, duration: 500, delay: 300 }}
