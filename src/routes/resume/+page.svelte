f0d418ce679e3ed69349f4f94d9c47a3923506e4
Mark Lund <marklund117@gmail.com>
2025-01-10 16:23:24 -0700

    fixes to resume button

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,7 +1,7 @@
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="w-full lg:w-[70rem] flex flex-col align-middle items-center p-4">
+<div class="container h-full mx-auto flex flex-row justify-center">
+	<div class="w-full lg:w-[70rem] flex flex-col justify-start items-center p-4">
 		<a
-			class="shadow-xl btn bg-gradient-to-br variant-gradient-primary-secondary w-full lg:max-w-[850px] h-[48px] mb-10"
+			class="shadow-xl btn bg-gradient-to-br variant-gradient-primary-secondary w-full lg:max-w-[850px] h-[48px] mb-10 mt-7"
 			href="/src/lib/documents/mark_lund_resume.pdf"
             target="_blank"
 			rel="noreferrer"
