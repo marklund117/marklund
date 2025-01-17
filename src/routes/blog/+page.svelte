8c53da3d06b14278db0707bf4542380eadd087a3
Mark Lund <marklund117@gmail.com>
2025-01-17 10:54:57 -0700

    blog mockup starts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,3 +1,22 @@
 <div class="container h-full mx-auto flex justify-center items-center">
-    <h1 class="text-black text-lg">Coming Soon!</h1>
+    <div class="w-full lg:w-[70rem] grid grid-cols-1 md:grid-cols-2">
+        <a href="/" target="blank">
+			<div class=" bg-white rounded-lg shadow-xl flex flex-col items-center m-4 card-hover">
+				<header class="card-header text-neutral-900 text-xl">Article Title</header>
+				<section class="p-4">
+					<img class="rounded-lg" src="namegencard.png" alt="a stylized green n logo on a black background" />
+				</section>
+				<footer class="text-neutral-900 pb-4">Description Text</footer>
+			</div>
+		</a>
+        <a href="/" target="blank">
+			<div class=" bg-white rounded-lg shadow-xl flex flex-col items-center m-4 card-hover">
+				<header class="card-header text-neutral-900 text-xl">Article Title</header>
+				<section class="p-4">
+					<img class="rounded-lg" src="namegencard.png" alt="a stylized green n logo on a black background" />
+				</section>
+				<footer class="text-neutral-900 pb-4">Description Text</footer>
+			</div>
+		</a>
+    </div> 
 </div>
