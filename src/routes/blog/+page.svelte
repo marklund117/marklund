85d3704bd1d2b41dba82dbff94b5cda7ef13cc59
Mark Lund <marklund117@gmail.com>
2025-01-29 14:20:31 -0700

    articles now have titles and link to Medium

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,21 +1,21 @@
 <div class="container h-full mx-auto flex justify-center items-center">
     <div class="w-full lg:w-[70rem] grid grid-cols-1 md:grid-cols-2">
-        <a href="/" target="blank">
+        <a href="https://medium.com/mark-lunds-portfolio/call-result-repeat-5c78181b1e2e" target="blank">
 			<div class=" bg-white rounded-lg shadow-xl flex flex-col items-center m-4 card-hover">
-				<header class="card-header text-neutral-900 text-xl">Article Title</header>
+				<header class="card-header text-neutral-900 text-xl">Call. Result. Repeat.</header>
 				<section class="p-4">
-					<img class="rounded-lg" src="namegencard.png" alt="a stylized green n logo on a black background" />
+					<img class="rounded-lg" src="dialerframe.png" alt="a stylized green n logo on a black background" />
 				</section>
-				<footer class="text-neutral-900 pb-4">Description Text</footer>
+				<footer class="text-neutral-900 pb-4">Redesigning a web-based phone software.</footer>
 			</div>
 		</a>
         <a href="/" target="blank">
 			<div class=" bg-white rounded-lg shadow-xl flex flex-col items-center m-4 card-hover">
-				<header class="card-header text-neutral-900 text-xl">Article Title</header>
+				<header class="card-header text-neutral-900 text-xl">The Importance of User Research</header>
 				<section class="p-4">
-					<img class="rounded-lg" src="namegencard.png" alt="a stylized green n logo on a black background" />
+					<img class="rounded-lg" src="researchboard.png" alt="a stylized green n logo on a black background" />
 				</section>
-				<footer class="text-neutral-900 pb-4">Description Text</footer>
+				<footer class="text-neutral-900 pb-4">Products exist for People</footer>
 			</div>
 		</a>
     </div> 
