27682f9d56a9fbeab859ad6dbd946ce48710c774
Mark Lund <marklund117@gmail.com>
2025-02-10 17:10:11 -0700

    _blank instead of blank

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,6 @@
 <div class="container h-full mx-auto flex justify-center items-center">
     <div class="w-full lg:w-[70rem] grid grid-cols-1 md:grid-cols-2">
-        <a href="https://medium.com/mark-lunds-portfolio/call-result-repeat-5c78181b1e2e" target="blank">
+        <a href="https://medium.com/mark-lunds-portfolio/call-result-repeat-5c78181b1e2e" target="_blank">
 			<div class=" bg-white rounded-lg shadow-xl flex flex-col items-center m-4 card-hover">
 				<header class="card-header text-neutral-900 text-xl">Call. Result. Repeat.</header>
 				<section class="p-4">
@@ -9,7 +9,7 @@
 				<footer class="text-neutral-900 pb-4">Redesigning a web-based phone software</footer>
 			</div>
 		</a>
-        <a href="https://medium.com/mark-lunds-portfolio/the-importance-of-user-research-6a7905286455" target="blank">
+        <a href="https://medium.com/mark-lunds-portfolio/the-importance-of-user-research-6a7905286455" target="_blank">
 			<div class=" bg-white rounded-lg shadow-xl flex flex-col items-center m-4 card-hover">
 				<header class="card-header text-neutral-900 text-xl">The Importance of User Research</header>
 				<section class="p-4">
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,6 @@
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="w-full lg:w-[70rem] grid grid-cols-1 md:grid-cols-2">
-		<a href="https://namegen-six.vercel.app/" target="blank">
+		<a href="https://namegen-six.vercel.app/" target="_blank">
 			<div class=" bg-white rounded-lg shadow-xl flex flex-col items-center m-4 card-hover">
 				<header class="card-header text-neutral-900 text-xl">Namegen</header>
 				<section class="p-4">
@@ -9,7 +9,7 @@
 				<footer class="text-neutral-900 pb-4">AI powered username generation tool.</footer>
 			</div>
 		</a>
-		<a href="https://www.adviserelite.com/" target="blank">
+		<a href="https://www.adviserelite.com/" target="_blank">
 			<div class=" bg-white rounded-lg shadow-xl flex flex-col items-center m-4 card-hover">
 				<header class="card-header text-neutral-900 text-xl">Adviser Elite</header>
 				<section class="p-4">
@@ -18,7 +18,7 @@
 				<footer class="text-neutral-900 pb-4">Professional site for a relative.</footer>
 			</div>
 		</a>
-		<a href="https://3620final.vercel.app/" target="blank">
+		<a href="https://3620final.vercel.app/" target="_blank">
 			<div class=" bg-white rounded-lg shadow-xl flex flex-col items-center m-4 card-hover">
 				<header class="card-header text-neutral-900 text-xl">Lorebook</header>
 				<section class="p-4">
