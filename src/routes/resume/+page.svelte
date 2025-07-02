a0fdddc6b516fd8329186b2619e77580490c20aa
Mark Lund <marklund117@gmail.com>
2025-07-02 13:04:01 -0600

    put pdf in iframe for now.

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -3,11 +3,18 @@
 		<a
 			class="shadow-xl btn bg-gradient-to-br variant-gradient-primary-secondary w-full lg:max-w-[850px] h-[48px] mb-10 mt-7"
 			href="resume_v3.pdf"
-            target="_blank"
+			target="_blank"
 			rel="noreferrer"
 		>
 			View/Download PDF
 		</a>
-		<img class="w-full lg:max-w-[850px]" src="resume_png_v3.png" alt="resume" />
+		<!-- <img class="w-full lg:max-w-[850px]" src="resume_png_v3.png" alt="resume" /> -->
+		<iframe
+			title="resume viewer"
+			src="resume_v3.pdf"
+			width="850px"
+			height="1100px"
+			style="border: none;"
+		></iframe>
 	</div>
 </div>
